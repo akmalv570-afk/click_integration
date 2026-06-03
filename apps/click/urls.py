c8389bf90aab0f512c94d7a944b2952b6fec6181
Akmal <akmalv570@gmail.com>
2026-06-03 13:56:34 +0500

    commit

--- apps/click/urls.py
+++ apps/click/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ClickWebhookView
+from .views import ClickWebhookView, InvoiceCreateView
 
 urlpatterns = [
     path('webhook/', ClickWebhookView.as_view(), name='click_webhook'),
+    path('invoice/create/', InvoiceCreateView.as_view(), name='invoice_create'),
 ]
--- apps/click/views.py
+++ apps/click/views.py
@@ -1,3 +1,7 @@
+from django.conf import settings
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from paytechuz.gateways.click import ClickGateway
 from paytechuz.integrations.django.views import BaseClickWebhookView
 from .models import Invoice
 
@@ -12,3 +16,35 @@ class ClickWebhookView(BaseClickWebhookView):
         invoice = Invoice.objects.get(id=transaction.account_id)
         invoice.status = 'cancelled'
         invoice.save()
+
+
+class InvoiceCreateView(APIView):
+    def post(self, request):
+        amount = request.data.get('amount')
+
+        if not amount:
+            return Response({'error': 'amount required'}, status=400)
+
+        invoice = Invoice.objects.create(
+            user=request.user,
+            amount=amount,
+            status='pending'
+        )
+
+        gateway = ClickGateway(
+            service_id=settings.PAYTECHUZ['CLICK']['SERVICE_ID'],
+            merchant_id=settings.PAYTECHUZ['CLICK']['MERCHANT_ID'],
+            merchant_user_id=settings.PAYTECHUZ['CLICK']['MERCHANT_USER_ID'],
+            secret_key=settings.PAYTECHUZ['CLICK']['SECRET_KEY'],
+            is_test_mode=settings.PAYTECHUZ['CLICK']['IS_TEST_MODE']
+        )
+
+        payment_url = gateway.create_payment(
+            id=invoice.id,
+            amount=invoice.amount,
+        )
+
+        return Response({
+            'invoice_id': invoice.id,
+            'payment_url': payment_url
+        }, status=201)
